bb6003e163cb1e817aeeb289996c856e34bd5b85
Abhinandan Arya <abhinandanarya06122000@gmail.com>
2021-01-23 13:20:28 +0530

    User signin completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/", to: "home#index"
   resources "users"
+  resources "orders"
+  resources "menus"
+  resources "order_items"
+  resources "menu_items"
+
+  get "/signin" => "sessions#new", as: :new_signin
+  post "/signin" => "sessions#create", as: :signin
+  delete "/signout" => "sessions#destroy", as: :destroy_session
 end
