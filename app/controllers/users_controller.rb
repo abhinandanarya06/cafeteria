213dd2668388f573be3d0aa1f7e9550526bc5115
Abhinandan Arya <abhinandanarya06122000@gmail.com>
2021-01-23 13:08:27 +0530

    User account creation finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+  skip_before_action :ensure_user_logged_in
+
+  def new
+    render "users/new"
+  end
+
+  def create
+    name = params[:name]
+    role = params[:role]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.new(
+      name: name,
+      role: role,
+      email: email,
+      password: password,
+    )
+    if new_user.save
+      session[:current_user_id] = new_user.id
+      @current_user = current_user
+      redirect_to "/"
+    else
+      flash[:error] = new_user.errors.full_messages.join(", ")
+      redirect_to new_user_path
+    end
+  end
 end
