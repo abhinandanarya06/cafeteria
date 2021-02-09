b8d76b178929abd840b12aedb4fd1e020e1b0657
Abhinandan Arya <abhinandanarya06122000@gmail.com>
2021-02-09 14:28:52 +0530

    Add sales report filter

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,6 +2,15 @@ class ReportsController < ApplicationController
   before_action :ensure_owner_or_clerk
 
   def index
-    render "index"
+    idate = params[:idate]
+    fdate = params[:fdate]
+    begin
+      idate = DateTime.parse(idate)
+      fdate = DateTime.parse(fdate)
+    rescue
+      idate = nil
+      fdate = nil
+    end
+    render "index", locals: { idate: idate, fdate: fdate }
   end
 end
